6c3a793aaae00b0b11e8361c2104bcc553d4a8f4
PKTruong-Talang <phungkhactruong2006@gmail.com>
2026-04-16 20:55:34 +0700

    xong TH2

--- config/routes.ts
+++ config/routes.ts
@@ -51,12 +51,14 @@
 	{
         path: '/th1',
         name: 'TH1 bài1',
-        component: './TH1' ,   
+		icon: 'TableOutlined',
+        component: './BAI1TH1' ,   
     },
 	{
         path: '/th2',
-        name: 'TH1 bài2',
-        component: './TH2' ,   
+        name: 'TH1 bài2',  
+		icon: 'TableOutlined',
+        component: './BAI2TH1' ,  
     },
 	{
         path: '/thuc-hanh-1',
